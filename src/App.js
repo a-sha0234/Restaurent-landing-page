7c12494c9e59d8cc85b20c62e2b825ffd89bad1d
a-sha0234 <asharma_31@hotmail.com>
2022-06-07 23:23:33 +0100

    update app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import dishes from "./dishesData";
+import SignatureDishes from "./components/SigntureDishes";
+import OpeningHours from "./components/OpeningHours";
+import ContactForm from "./components/ContactForm";
 import "../src/scss/main.scss";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
+
+      {dishes.map((dish) => {
+        return <SignatureDishes dish={dish} />;
+      })}
+      <OpeningHours />
+      <ContactForm />
     </div>
   );
 }
