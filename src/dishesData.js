6b3679d05126f31eb231eb9139bb16bf7918fb68
a-sha0234 <asharma_31@hotmail.com>
2022-06-07 23:23:00 +0100

    create dish data

---
+++ src/dishesData.js
@@ -0,0 +1,32 @@
+import peperoni from "./images/peparoniPizza.png";
+import vege from "./images/vege.jpg";
+import bbq from "./images/bbq.jpg";
+
+const arr = [
+  {
+    dishNo: 1,
+    name: "Peperoni",
+    url: peperoni,
+    description:
+      "The finest home made sizzling peparoni pizza served with a side dish of garlic bread. Topping include cheese, peparoni and basel",
+  },
+
+  {
+    dishNo: 2,
+    name: "Spicy Vegetarian",
+    url: vege,
+    description:
+      "A tasty meat free pizza including the following toppings, onion, mushrooms, pepper and tomato ",
+  },
+  {
+    dishNo: 3,
+    name: "Texas bbq pizza",
+    url: bbq,
+    description:
+      "A texas take on a pizza which includes cheese, chicken. mushrooms and pepper topped with bbq sauce ",
+  },
+];
+
+export default arr;
+
+// "https://www.simplehomecookedrecipes.com/italian-pepperoni-pizza-recipe/"
