40ce4c9527097ad0abf2431d896bf98ab42e5cd2
a-sha0234 <asharma_31@hotmail.com>
2022-06-07 23:17:26 +0100

    create component for opening hours

---
+++ src/components/OpeningHours.js
@@ -0,0 +1,36 @@
+export default function OpeningHours() {
+  return (
+    <section className="OpeningTimes" id="openTimes">
+      <h1>Opening Times</h1>
+      <ul>
+        <li>
+          <p>Monday: 3pm - 9:30pm</p>
+        </li>
+        <li>
+          {" "}
+          <p>Tuesday: 3pm - 9:30pm</p>
+        </li>
+        <li>
+          {" "}
+          <p>Wednesday: 3pm - 9:30pm</p>
+        </li>
+        <li>
+          {" "}
+          <p>Thursday: 3pm - 9:30pm</p>
+        </li>
+        <li>
+          {" "}
+          <p>Friday: 3pm - 11:00pm</p>
+        </li>
+        <li>
+          {" "}
+          <p>Saturday: 3pm - 11:00pm</p>
+        </li>
+        <li>
+          {" "}
+          <p>Sunday CLOSED</p>
+        </li>
+      </ul>
+    </section>
+  );
+}
